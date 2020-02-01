c663766b18228d5458446336914d1216f814c78a
alselwi2018 <abdul@Abdus-MacBook-Pro.local>
2020-02-01 10:28:14 +0000

    youtube-channels

---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\channel;
+use Faker\Generator as Faker;
+
+$factory->define(channel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
